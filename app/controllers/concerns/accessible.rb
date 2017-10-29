160848bc51fc17d839315e729956096790b570dc
Rashmi <you@example.com>
2017-10-29 16:18:59 +0530

    changes

--- app/controllers/concerns/accessible.rb
+++ app/controllers/concerns/accessible.rb
@@ -1,20 +1,20 @@
-module Accessible
-  extend ActiveSupport::Concern
-  included do
-    before_action :check_vendor
-  end
+# module Accessible
+#   extend ActiveSupport::Concern
+#   included do
+#     before_action :check_vendor
+#   end
 
-  protected
-  def check_vendor
-    if current_manufacture
-      flash.clear
-      # if you have rails_admin. You can redirect anywhere really
-      redirect_to(manufacture_session_path) && return
+#   protected
+#   def check_vendor
+#     if current_manufacture
+#       flash.clear
+#       # if you have rails_admin. You can redirect anywhere really
+#       redirect_to(manufacture_session_path) && return
 
-      elsif current_vendor
-      flash.clear
-      # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
-      redirect_to(authenticated_vendor_root_path) && return
-    end
-  end
-end
+#       elsif current_vendor
+#       flash.clear
+#       # The authenticated root path can be defined in your routes.rb in: devise_scope :user do...
+#       redirect_to(authenticated_vendor_root_path) && return
+#     end
+#   end
+# end
